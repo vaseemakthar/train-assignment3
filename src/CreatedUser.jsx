cf1ca2c9fcbbb1a195be3dc546500db03cc42176
VASEEM AKTHAR A <vaseemakthar31@gmail.com>
2025-01-06 13:41:54 +0530

    versal added

--- src/CreatedUser.jsx
+++ src/CreatedUser.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
+// import './CreatedUser.css';
 import axios from 'axios';
 
 function CreatedUser(){
@@ -18,17 +19,52 @@ function CreatedUser(){
  }
 
   return(
-    <div>
-      <form onSubmit={submit}>
-      <label>name</label>
-       <input type="text" onChange={(e)=>setName(e.target.value)}/><br/>
-       <label>email</label>
-       <input type="text" onChange={(e)=>setEmail(e.target.value)}/><br/>
-       <label>address</label>
-       <input type="text" onChange={(e)=>setAddress(e.target.value)}/>
-       <button type="submit">create</button>
-      </form>
+    // <div>
+    //   <form onSubmit={submit}>
+    //   <label>name</label>
+    //    <input type="text" onChange={(e)=>setName(e.target.value)}/><br/>
+    //    <label>email</label>
+    //    <input type="text" onChange={(e)=>setEmail(e.target.value)}/><br/>
+    //    <label>address</label>
+    //    <input type="text" onChange={(e)=>setAddress(e.target.value)}/>
+    //    <button type="submit">create</button>
+    //   </form>
       
+    // </div>
+    <div className="created-user-container">
+      <form className="created-user-form" onSubmit={submit}>
+        <h2 className="form-title">Create User</h2>
+        <div className="form-group">
+          <label className="form-label">Name</label>
+          <input
+            className="form-input"
+            type="text"
+            placeholder="Enter name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Email</label>
+          <input
+            className="form-input"
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Address</label>
+          <input
+            className="form-input"
+            type="text"
+            placeholder="Enter address"
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </div>
+        <button className="form-submit-button" type="submit">
+          Create
+        </button>
+      </form>
     </div>
   )
 }
